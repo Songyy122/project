63bdfd91d355daf26816b54507e1cbc95f380b34
songyy <3296737380@qq.com>
2020-09-29 10:43:40 +0800

    项目初始化

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+App.mpType = 'app'
+
+const app = new Vue(App)
+app.$mount()
+
+export default {
+// 这部分相当于原生小程序的 app.json
+  config: {
+    pages: [],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: '第一个小程序',
+      navigationBarTextStyle: 'black'
+    }
+  }
+}
---
+++ src/pages/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="message">{{msg}}</div>
+    <!-- <vue-qr :logoSrc="imageUrl" text="xxx" :size="200"></vue-qr> -->
+  </div>
+</template>
+
+<script>
+// import vueQr from 'vue-qr'
+export default {
+  name: 'qecode',
+  data() {
+    return {
+      imageUrl: '',
+      msg: 'hhh',
+    };
+  },
+  // components: {
+  //   vueQr
+  // },
+};
+</script>
+
+<style scoped>
+.message {
+  color: red;
+  padding: 10px;
+  text-align: center;
+}
+</style>
